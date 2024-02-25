c60779ef5ebdd1b3118d36ad498cb6f4091e271a
luizfiller <luiz.fsilva@sptech.school>
2024-02-25 17:59:13 -0300

    chore: init MainActivity w/ View + Toast

---
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem vindo(a) ao Orgs!", Toast.LENGTH_SHORT).show()
+
+        val view = View(this)
+
+        setContentView(view)
+    }
+
+}
